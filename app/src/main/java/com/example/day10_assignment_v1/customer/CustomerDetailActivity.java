cf4e9e98304de87a55585efb935b566618b8e0f3
TeaTammiste <55560458+TeaTammiste@users.noreply.github.com>
2019-09-26 10:10:56 -0600

    Customer Class

--- app/src/main/java/com/example/day10_assignment_v1/customer/CustomerDetailActivity.java
+++ app/src/main/java/com/example/day10_assignment_v1/customer/CustomerDetailActivity.java
@@ -3,14 +3,33 @@ package com.example.day10_assignment_v1.customer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.day10_assignment_v1.R;
 
 public class CustomerDetailActivity extends AppCompatActivity {
 
+    EditText etCustomerId, etCustFirstName, etCustLastName, etCustAddress,
+            etCustCity, etCustProv, etCustPostal, etCustCountry, etCustHomePhone,
+            etCustBusPhone, etCustEmail;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_detail);
+
+        etCustomerId = findViewById(R.id.etCustomerId);
+        etCustFirstName=findViewById(R.id.etCustFirstName);
+        etCustLastName = findViewById(R.id.etCustLastName);
+        etCustAddress=findViewById(R.id.etCustAddress);
+        etCustCity = findViewById(R.id.etCustCity);
+        etCustProv=findViewById(R.id.etCustProv);
+        etCustPostal = findViewById(R.id.etCustPostal);
+        etCustCountry=findViewById(R.id.etCustCountry);
+        etCustHomePhone = findViewById(R.id.etCustHomePhone);
+        etCustBusPhone=findViewById(R.id.);
+        etCustomerId = findViewById(R.id.etCustomerId);
+        etCustFirstName=findViewById(R.id.etCustFirstName);
     }
 }
--- app/src/main/java/com/example/day10_assignment_v1/customer/CustomerListActivity.java
+++ app/src/main/java/com/example/day10_assignment_v1/customer/CustomerListActivity.java
@@ -1,16 +1,26 @@
-package com.example.day10_assignment_v1.booking;
+package com.example.day10_assignment_v1.customer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.day10_assignment_v1.R;
 
 public class CustomerListActivity extends AppCompatActivity {
+    ListView lvCustomers;
+    Button btnNewCustomer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_list);
+
+
+        lvCustomers = findViewById(R.id.lvCustomers);
+        btnNewCustomer = findViewById(R.id.btnNewCustomer);
+
+
     }
 }
