2fb0f96d14344b0098336328854fc567383608be
Jay Gervais <mrjaygervais@gmail.com>
2019-09-26 07:28:31 -0600

    Added DB Helper to reduce code duplication.

---
+++ app/src/main/java/com/example/day10_assignment_v1/DBHelper.java
@@ -0,0 +1,30 @@
+package com.example.day10_assignment_v1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class DBHelper
+{
+    // input stream used in multiple queries
+    public static String urlInputStream(String urlWebService)
+    {
+        try
+        {
+            URL url = new URL(urlWebService);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            StringBuilder sb = new StringBuilder();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String json;
+            while ((json = bufferedReader.readLine()) != null)
+            {
+                sb.append(json + "\n");
+            }
+            return sb.toString().trim();
+        } catch (Exception e)
+        {
+            return null;
+        }
+    }
+}
